a9c17edfd807fd938500e3428e0a2e9f65703464
kitayama <shingo.kitayama@gmail.com>
2025-02-22 15:35:49 +0900

    ref: Func.php 共通部分をまとめてPHPUnit追加

--- app/Keys/Func.php
+++ app/Keys/Func.php
@@ -3,51 +3,31 @@
 	namespace sin5ddd\SQBuilder\Keys;
 	
 	class Func extends ISQLKey {
-		static public function count(string $key, ?string $as):string {
-			$ret = "COUNT($key)";
-			if($as){
-				$ret .= " AS $as";
-			}
-			return $ret;
+		static public function count(string $key, ?string $as=null):string {
+			return self::bake("COUNT($key)",$as);
 		}
 		
-		static public function count_distinct(string $key, ?string $as):string {
-			$ret = "COUNT(DISTINCT($key))";
-			if($as){
-				$ret .= " AS $as";
-			}
-			return $ret;
+		static public function count_distinct(string $key, ?string $as=null):string {
+			return self::bake("COUNT(DISTINCT($key))",$as);
 		}
 		
-		static public function sum(string $key, ?string $as):string {
-			$ret = "SUM($key)";
-			if($as){
-				$ret .= " AS $as";
-			}
-			return $ret;
+		static public function sum(string $key, ?string $as=null):string {
+			return self::bake("SUM($key)",$as);
 		}
 		
-		static public function max(string $key, ?string $as):string {
-			$ret = "MAX($key)";
-			if($as){
-				$ret .= " AS $as";
-			}
-			return $ret;
+		static public function max(string $key, ?string $as=null):string {
+			return self::bake("MAX($key)",$as);
 		}
 		
-		static public function min(string $key, ?string $as):string {
-			$ret = "MIN($key)";
-			if($as){
-				$ret .= " AS $as";
-			}
-			return $ret;
+		static public function min(string $key, ?string $as=null):string {
+			return self::bake("MIN($key)",$as);
 		}
 		
-		static public function avg(string $key, ?string $as):string {
-			$ret = "AVG($key)";
-			if($as){
-				$ret .= " AS $as";
-			}
-			return $ret;
+		static public function avg(string $key, ?string $as=null):string {
+			return self::bake("AVG($key)",$as);
+		}
+		
+		static private function bake(string $fn,?string $as):string {
+			return $as ? $fn." AS $as" : $fn;
 		}
 	}
