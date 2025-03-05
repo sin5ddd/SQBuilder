91c7e1e990513c26b68b45c158f7673c734bc07a
kitayama <shingo.kitayama@gmail.com>
2025-03-05 12:42:05 +0900

    fix(feat-save-to-file): CUI用のメソッドを削除

--- app/Builder/Interface/IBuilder.php
+++ app/Builder/Interface/IBuilder.php
@@ -10,56 +10,10 @@
 		protected string $method;
 		protected string $key;
 		
-		
-		
-		private function parse(array $arguments): array {
-			$params = [];
-			foreach ($arguments as $arg) {
-				if (is_array($arg)) {
-					$params[] = implode(',', $this->parse($arg));
-				} else if (is_object($arg) && get_class($arg) === get_class($this)) {
-					$params[] = sprintf('(%s)', $arg->build());
-				} else {
-					$params[] = $arg;
-				}
-			}
-			return $params;
-		}
-		
-		public function __call($name, $arguments) {
-			if (strtolower($name) == 'and') {
-				$this->where[] = implode(' ', $arguments);
-			} else {
-				$this->params = array_merge(
-					$this->params, $this->parse(array_merge(array_map('strtoupper', explode('_', $name)), $arguments)));
-			}
-			return $this;
-		}
-		
-		public function order_direction(string $direction): self {
-			$this->order_direction = $direction;
-			return $this;
-		}
-		
-		public function order(?string $order_column, int $direction = 0): self {
-			if (!empty($order_column)) {
-				if ($direction > 0) {
-					$dir = ' ASC';
-				} else {
-					$dir = ' DESC';
-				}
-				$this->order[] = $order_column . $dir;
-			}
-			return $this;
-		}
-		
-		public function group(?string $group_column): self {
-			if (!empty($group_column)) {
-				$this->group[] = $group_column;
-			}
-			return $this;
-		}
-		
+		/**
+		 * $this->methodを必ず指定する
+		 */
+		abstract public function __construct();
 		
 		/**
 		 * SQL文書き出し
