0e09cc3adff918bbd88f62bc72933093ba3d3ddb
kitayama <shingo.kitayama@gmail.com>
2025-03-08 13:25:26 +0900

    refactor(PHPDoc): setting PHPDoc

--- app/Keys/Func.php
+++ app/Keys/Func.php
@@ -3,35 +3,100 @@
 	namespace sin5ddd\SQBuilder\Keys;
 	
 	class Func extends ISQLKey {
+		/**
+		 * returns "COUNT($key) AS $as" SQL Function statement
+		 * when $as is not set, returns without "AS".
+		 * @param string      $key
+		 * @param string|null $as
+		 *
+		 * @return string
+		 */
 		static public function count(string $key, ?string $as=null):string {
 			return self::bake("COUNT($key)",$as);
 		}
 		
+		/**
+		 *  returns "COUNT(DISTINCT($key)) AS $as" SQL Function statement
+		 *  when $as is not set, returns without 'AS'.
+		 *
+		 * @param string      $key
+		 * @param string|null $as
+		 *
+		 * @return string
+		 */
 		static public function count_distinct(string $key, ?string $as=null):string {
 			return self::bake("COUNT(DISTINCT($key))",$as);
 		}
 		
+		/**
+		 * returns "SUM($key) AS $as" SQL Function statement
+		 * when $as is not set, returns without 'AS'.
+		 *
+		 * @param string      $key
+		 * @param string|null $as
+		 *
+		 * @return string
+		 */
 		static public function sum(string $key, ?string $as=null):string {
 			return self::bake("SUM($key)",$as);
 		}
 		
+		/**
+		 * returns "MAX($key) AS $as" SQL Function statement
+		 * when $as is not set, returns without 'AS'.
+		 *
+		 * @param string      $key
+		 * @param string|null $as
+		 *
+		 * @return string
+		 */
 		static public function max(string $key, ?string $as=null):string {
 			return self::bake("MAX($key)",$as);
 		}
 		
+		/**
+		 *  returns "MIN($key) AS $as" SQL Function statement
+		 *  when $as is not set, returns without 'AS'.
+		 *
+		 * @param string      $key
+		 * @param string|null $as
+		 *
+		 * @return string
+		 */
 		static public function min(string $key, ?string $as=null):string {
 			return self::bake("MIN($key)",$as);
 		}
 		
+		/**
+		 * returns "AVG($key) AS $as" SQL Function statement
+		 * when $as is not set, returns without 'AS'.
+		 * @param string      $key
+		 * @param string|null $as
+		 *
+		 * @return string
+		 */
 		static public function avg(string $key, ?string $as=null):string {
 			return self::bake("AVG($key)",$as);
 		}
 		
-		static private function bake(string $fn,?string $as):string {
-			return $as ? $fn." AS $as" : $fn;
-		}
-		
+		/**
+		 * returns simply "TIMESTAMP()" SQL Function statement
+		 *
+		 * @return string
+		 */
 		static private function timestamp():string{
 			return self::bake("TIMESTAMP()",null);
+		}
+		
+		/**
+		 * Build and return statement SQL Function statement
+		 *
+		 * @param string      $fn
+		 * @param string|null $as
+		 *
+		 * @return string
+		 */
+		static private function bake(string $fn, ?string $as):string {
+			return $as ? $fn." AS $as" : $fn;
 		}
 	}
--- app/Keys/Where.php
+++ app/Keys/Where.php
@@ -5,30 +5,90 @@
 	use sin5ddd\SQBuilder\Helper\EscapeValues;
 	
 	class Where extends ISQLKey {
+		/**
+		 * Compare with '='.
+		 * @param string           $key column name
+		 * @param string|int|float $value field value
+		 *
+		 * @return string
+		 */
 		static public function equal(string $key, string|int|float $value): string {
 			return "$key = ".EscapeValues::check_str($value);
 		}
 		
+		/**
+		 * Compare field and value with '<>'
+		 * @param string           $key
+		 * @param string|int|float $value
+		 *
+		 * @return string
+		 */
 		static public function notEqual(string $key, string|int|float $value): string {
 			return "$key <> ".EscapeValues::check_str($value);
 		}
 		
+		/**
+		 * Compare field and value with '>'
+		 * value must be integer or float.
+		 *
+		 * @param string    $key
+		 * @param int|float $value
+		 *
+		 * @return string
+		 */
 		static public function greater(string $key, int|float $value): string {
 			return "$key > $value";
 		}
 		
+		/**
+		 * Compare field and value with '>='
+		 * value must be integer or float.
+		 *
+		 * @param string    $key
+		 * @param int|float $value
+		 *
+		 * @return string
+		 */
 		static public function greaterEqual(string $key, int|float $value): string {
 			return "$key >= $value";
 		}
 		
+		/**
+		 * Compare field and value with '<'
+		 * value must be integer or float.
+		 *
+		 * @param string    $key
+		 * @param int|float $value
+		 *
+		 * @return string
+		 */
 		static public function less(string $key, int|float $value): string {
 			return "$key < $value";
 		}
 		
+		/**
+		 * Compare field and value with '<='
+		 * value must be integer or float.
+		 *
+		 * @param string    $key
+		 * @param int|float $value
+		 *
+		 * @return string
+		 */
 		static public function lessEqual(string $key, int|float $value): string {
 			return "$key <= $value";
 		}
 		
+		/**
+		 * Compare field and values with 'IN' statement
+		 * values must be integer|float|string.
+		 *
+		 * @param string $key
+		 * @param array<int|float|string> $values
+		 *
+		 * @return string
+		 * @throws \Exception
+		 */
 		static public function in(string $key, array $values): string {
 			if(sizeof($values) < 2) {
 				throw new \Exception("IN statement needs at least 2 values");
@@ -38,6 +98,16 @@
 			return "$key IN ($arr_str)";
 		}
 		
+		/**
+		 * Compare field and value with 'NOT IN' statement.
+		 * values must be integer|float|string.
+		 *
+		 * @param string $key
+		 * @param array<int|float|string>  $values
+		 *
+		 * @return string
+		 * @throws \Exception
+		 */
 		static public function notIn(string $key, array $values): string {
 			if(sizeof($values) < 2) {
 				throw new \Exception("NOT IN statement needs at least 2 values");
@@ -47,6 +117,16 @@
 			return "$key NOT IN ($arr_str)";
 		}
 		
+		/**
+		 * Compare field and value with 'BETWEEN xx AND yy' statement.
+		 * values must be integer or float, and accept only 2 values.
+		 *
+		 * @param string $key
+		 * @param array<int|float>  $values
+		 *
+		 * @return string
+		 * @throws \Exception
+		 */
 		static public function between(string $key, array $values): string {
 			if(sizeof($values) !== 2) {
 				throw new \Exception("Between statement accepts 2 values only");
@@ -56,6 +136,16 @@
 			return "$key BETWEEN $arr_str";
 		}
 		
+		/**
+		 * Compare field and value with 'NOT BETWEEN xx AND yy' statement.
+		 * values must be integer or float, and accept only 2 values.
+		 *
+		 * @param string $key
+		 * @param array  $values
+		 *
+		 * @return string
+		 * @throws \Exception
+		 */
 		static public function notBetween(string $key, array $values): string {
 			if(sizeof($values) !== 2) {
 				throw new \Exception("NOT Between statement accepts 2 values only");
